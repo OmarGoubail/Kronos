736428c7aa4dd453150dd695cc943ea43e7feb84
OmarGoubail <omarsayed223@gmail.com>
2024-10-10 23:12:35 +0300

    add the kronos theme

--- packages/kronos-app/src/components/Editor/index.tsx
+++ packages/kronos-app/src/components/Editor/index.tsx
@@ -1,48 +1,78 @@
-import { createSignal, onMount } from 'solid-js';
-import { createEditor, $getRoot, $getSelection, EditorState, LexicalEditor, $createParagraphNode } from 'lexical';
-import { registerDragonSupport } from '@lexical/dragon';
-import { createEmptyHistoryState, registerHistory } from '@lexical/history';
-import { HeadingNode, QuoteNode, registerRichText } from '@lexical/rich-text';
-import { mergeRegister } from '@lexical/utils';
+import { createEffect, createSignal, onMount } from "solid-js";
+import {
+	createEditor,
+	$getRoot,
+	$getSelection,
+	type EditorState,
+	type LexicalEditor,
+	$createParagraphNode,
+} from "lexical";
+import { registerDragonSupport } from "@lexical/dragon";
+import { createEmptyHistoryState, registerHistory } from "@lexical/history";
+import { HeadingNode, QuoteNode, registerRichText } from "@lexical/rich-text";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { CodeNode } from "@lexical/code";
+import { mergeRegister } from "@lexical/utils";
+import { LinkNode } from "@lexical/link";
+
+import {
+	$convertFromMarkdownString,
+	$convertToMarkdownString,
+	registerMarkdownShortcuts,
+	TRANSFORMERS,
+} from "@lexical/markdown";
+import { kronosTheme } from "./theme";
 
 export type InitialEditorStateType =
-  | null
-  | string
-  | EditorState
-  | ((editor: LexicalEditor) => void);
-
-const theme = {
-
-};
+	| null
+	| string
+	| EditorState
+	| ((editor: LexicalEditor) => void);
 
 const Editor = () => {
-  const [editor, setEditor] = createSignal<LexicalEditor | null>(null);
+	const [editor, setEditor] = createSignal<LexicalEditor | null>(null);
 
-  onMount(() => {
-    const lexicalEditor = createEditor({
-      namespace: 'MyEditor',
-      editable: true,
-      onError: (error) => console.error(error),
-      theme,
-    });
+	onMount(() => {
+		const lexicalEditor = createEditor({
+			namespace: "MyEditor",
+			editable: true,
+			onError: (error) => console.error(error),
+			theme: kronosTheme,
+			nodes: [
+				HeadingNode,
+				QuoteNode,
+				ListNode,
+				ListItemNode,
+				CodeNode,
+				LinkNode,
+			],
+		});
 
-    setEditor(lexicalEditor);
+		setEditor(lexicalEditor);
 
-    const rootElement = document.getElementById('editor');
-    lexicalEditor.setRootElement(rootElement);
+		const rootElement = document.getElementById("editor");
+		lexicalEditor.setRootElement(rootElement);
 
-    mergeRegister(
-      registerRichText(lexicalEditor),
-      registerDragonSupport(lexicalEditor),
-      registerHistory(lexicalEditor, createEmptyHistoryState(), 300),
-    );
-  });
+		mergeRegister(
+			registerRichText(lexicalEditor),
+			registerDragonSupport(lexicalEditor),
+			registerHistory(lexicalEditor, createEmptyHistoryState(), 300),
+			registerMarkdownShortcuts(lexicalEditor, TRANSFORMERS),
+		);
 
-  return (
-    <div>
-      <div id="editor" contentEditable={true} />
-    </div>
-  );
+		lexicalEditor.update(() => {
+			$convertFromMarkdownString(
+				"# Welcome to Markdown\n\nThis is a paragraph.",
+				TRANSFORMERS,
+			);
+		});
+	});
+
+	return (
+		<div class="p-20">
+			<div id="editor" contentEditable={true} />
+		</div>
+	);
 };
 
 export default Editor;
